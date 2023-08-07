b28b600bb3281d76051f9ec930b789309e934a5c
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2023-08-07 16:02:07 -0300

    feat(UserGetRequest)

--- backend/index.ts
+++ backend/index.ts
@@ -2,13 +2,39 @@ import { Request, Response } from "express";
 
 import express from "express";
 
-import { PrismaClient } from '@prisma/client'; 
-const prisma = new  PrismaClient();
+import { PrismaClient, User, Analysis, Pet, Prisma} from '@prisma/client'; 
+
+const prisma: PrismaClient = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
+});
+
+app.get("/user/:userName", async (req: Request, res: Response) => {
+    const { userName } = req.params;
+    const user = await prisma.user.findUnique({
+        where: { username: userName },
+        select: {
+            ID: true,
+            username: true,
+            password: false,
+            email: true,
+            phone: true
+        }
+    }).catch((err: Prisma.PrismaClientKnownRequestError) => {
+        res.status(400).send(err.message)
+    });
+
+    if (!user)
+    {
+        res.status(404).send("User not found");
+        return;
+    }
+
+    res.status(200).json(user);
 });
 
 app.listen(3000, () => {
